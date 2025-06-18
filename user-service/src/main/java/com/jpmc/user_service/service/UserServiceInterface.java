a7b633ac659bedd4a50dd55d64d10b35bf134902
sheela miranda <sheelamiranda27@gmail.com>
2025-06-18 09:33:56 +0530

    userservice commit

---
+++ user-service/src/main/java/com/jpmc/user_service/service/UserServiceInterface.java
@@ -0,0 +1,13 @@
+package com.jpmc.user_service.service;
+
+import com.jpmc.user_service.dto.UserDto;
+import com.jpmc.user_service.entity.User;
+
+import java.util.List;
+
+public interface UserServiceInterface {
+
+    User createUser(User user);
+    List<User> getAllUsers();
+    User getUserById(Long id);
+}
