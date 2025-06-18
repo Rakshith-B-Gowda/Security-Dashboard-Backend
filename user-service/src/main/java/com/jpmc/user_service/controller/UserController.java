8bbea8634ff1624e9bd17bff9219900573c79a90
sheela miranda <sheelamiranda27@gmail.com>
2025-06-18 12:25:07 +0530

    userservice commit

--- user-service/src/main/java/com/jpmc/user_service/controller/UserController.java
+++ user-service/src/main/java/com/jpmc/user_service/controller/UserController.java
@@ -2,34 +2,39 @@ package com.jpmc.user_service.controller;
 
 import com.jpmc.user_service.entity.User;
 import com.jpmc.user_service.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @RestController
 public class UserController {
-    @Autowired
-    UserService service;
+   // @Autowired
+    UserService userService;
 
     @PostMapping("/adduser")
     public ResponseEntity<User> create(@RequestBody User user) {
-        User saved = service.createUser(user);
+        User saved = userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
     /* GET  /users  ── list all */
     @GetMapping("/all")
     public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok(service.getAllUsers());
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
     /* GET  /users/{id}  ── single */
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.getUserById(id));
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 
 
--- user-service/src/main/java/com/jpmc/user_service/service/UserService.java
+++ user-service/src/main/java/com/jpmc/user_service/service/UserService.java
@@ -4,24 +4,30 @@ import com.jpmc.user_service.dto.UserDto;
 import com.jpmc.user_service.entity.User;
 import com.jpmc.user_service.exception.UserNotFoundException;
 import com.jpmc.user_service.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserService implements UserServiceInterface{
-    @Autowired
-    UserRepository repo;
+   // @Autowired
+    UserRepository userRepository;
     public User createUser(User user) {
-        return repo.save(user);
+        return userRepository.save(user);
     }
 
     public List<User> getAllUsers() {
-        return repo.findAll();
+        return userRepository.findAll();
     }
 
     public User getUserById(Long id) {
-        return repo.findById(id)
+        return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
     }
 
